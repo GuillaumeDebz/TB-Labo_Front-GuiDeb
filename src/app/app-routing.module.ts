e2336c5e0ab1ecb052027f5b0f8d34f70a5e7bd4
GuillaumeDebz <guillaume.debruyne@gmail.com>
2022-10-12 20:11:20 +0200

    commit1 - bootstrap add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Page 404 //
+import { Four0FourComponent } from './TechnoCoop/four0four/four0four.component';
+
+// Home, NavBar & Footer //
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+
+
+
+
+
+
+
+
+
+
+
+  { path: "**", component: Four0FourComponent }
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './admin/users/users.component';
+import { Four0FourComponent } from './TechnoCoop/four0four/four0four.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    Four0FourComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
